f1155b597e94a7dae231c0fb56c3f5acdc193a47
arobbi7 <arobbins@azavea.com>
2017-02-16 09:18:50 -0500

    Lab 1 & Lab 2, part 1

--- lab/lab2/js/part1-ajax-calls.js
+++ lab/lab2/js/part1-ajax-calls.js
@@ -19,7 +19,7 @@
   This recipe, can be used by underscore's _.filter. It will return only words with
    >=5 characters.
 ===================== */
-var isLengthOfFiveOrMore = function(str) {};
+var isLengthOfFiveOrMore = function(str) {return str.length>=5;};
 
 console.log("isLengthOfFiveOrMore success:",
   _.isEqual(_.filter(['this', 'is','a', 'test', 'testing'], isLengthOfFiveOrMore), ['testing']));
@@ -30,15 +30,16 @@ console.log("isLengthOfFiveOrMore success:",
   function you write along with underscore's _.each to log the double of every
   number in the provided array.
 ===================== */
-var logDouble = function(num) {};
+var logDouble = function(num) {return num*2;};
 var theArray = [1, 5, 20, 100];
 
-
+console.log("doubling success:",
+  _.each(theArray, logDouble));
 /* =====================
   Given this already defined function, define fizzbuzzArray so that, when mapped
   over, it will equal ['fizz', 'buzz', 'fizzbuzz'];
 ===================== */
-var fizzbuzzArray = [];
+var fizzbuzzArray = [12,20, 15];
 var fizzbuzzFunc = function(num) {
   var str = '';
   if (num % 3 === 0) { str = 'fizz'; }
@@ -103,12 +104,20 @@ var phillyBikeCrashesDataUrl = "https://raw.githubusercontent.com/CPLN692-MUSA61
   Remember to call all code within the function body. Use console.log to make sure
   that this step is completed before moving on!
 ===================== */
-
+$.ajax("https://raw.githubusercontent.com/CPLN692-MUSA611/datasets/master/json/philadelphia-solar-installations.json").done(function(ajaxResponseValue) {
+  var data = JSON.parse(ajaxResponseValue);
+  console.log(data);
+  _.each(data,function(obj) {
+    // console.log([obj.LAT, obj.LONG_])
+    L.marker([obj.LAT, obj.LONG_]).addTo(map)
+  })
+});
 
 /* =====================
   Now that you've properly parsed your data, use _.each to plot the
   dataset you've pulled down.
 ===================== */
+//see above
 
 
 /* =====================
